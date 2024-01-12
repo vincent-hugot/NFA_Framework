b33114d1914cb24396d148e38ce310c1a20adda8
Vincent Hugot <vincent.hugot@gmail.com>
2024-01-12 09:25:28 +0100

    [M:arch_endeavouros.sh & M:debian_ubuntu.sh]

--- NFA_install_scripts/debian_ubuntu.sh
+++ NFA_install_scripts/debian_ubuntu.sh
@@ -1,10 +1,10 @@
 
 
 sudo apt install git graphviz pdftk python3-lark fonts-linuxlibertine \
-  python-lsp-server
+  python3-pyslp idle
 
 # get my framework, then run tests
 git clone https://github.com/vincent-hugot/NFA_Framework.git
 cd NFA_Framework/ || return
 ./tests.py
-xdg-open VISU.pdf
+xdg-open visu.pdf
