58cfa12be766fd46a86856e8139c5d761629d3ae
Vincent Hugot <vincent.hugot@gmail.com>
2024-01-12 09:33:40 +0100

    [M:arch_endeavouros.sh]

--- NFA_install_scripts/arch_endeavouros.sh
+++ NFA_install_scripts/arch_endeavouros.sh
@@ -1,7 +1,7 @@
 
 # update/upgrade system, then install packages
 sudo pacman -Syu
-sudo pacman -S git pdftk graphviz python tk xz python-pip mpdecimal \
+sudo pacman -S --needed git pdftk graphviz python tk xz python-pip mpdecimal \
   python-lsp-server python-lark-parser libertinus-font
 
 # get my framework, then run tests
