47b4b4f8d03b2dbf9db4e9f8fbd88156c51deac6
Vincent Hugot <vincent.hugot@gmail.com>
2024-01-12 09:17:15 +0100

    [M:debian_ubuntu.sh]

--- NFA_install_scripts/debian_ubuntu.sh
+++ NFA_install_scripts/debian_ubuntu.sh
@@ -1,6 +1,7 @@
 
 
-sudo apt install git graphviz pdftk python3-lark fonts-linuxlibertine
+sudo apt install git graphviz pdftk python3-lark fonts-linuxlibertine \
+  python-lsp-server
 
 # get my framework, then run tests
 git clone https://github.com/vincent-hugot/NFA_Framework.git
