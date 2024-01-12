93c54b1a712348194838e4a398653b61cb372297
Vincent Hugot <vincent.hugot@gmail.com>
2024-01-12 09:59:16 +0100

    [M:arch_endeavouros.sh]

--- NFA_install_scripts/arch_endeavouros.sh
+++ NFA_install_scripts/arch_endeavouros.sh
@@ -4,6 +4,9 @@ sudo pacman -Syu
 sudo pacman -S --needed git pdftk graphviz python tk xz python-pip mpdecimal \
   python-lsp-server python-lark-parser libertinus-font
 
+# LateX: deactivate for faster install
+sudo pacman -S --needed texlive-binextra texlive-core
+
 # get my framework, then run tests
 git clone https://github.com/vincent-hugot/NFA_Framework.git
 cd NFA_Framework/ || return
