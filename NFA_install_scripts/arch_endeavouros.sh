3733ea91990479aa34a944f0c08e2e31cb74aed3
Vincent Hugot <vincent.hugot@gmail.com>
2024-01-12 10:17:58 +0100

    [M:arch_endeavouros.sh & M:debian_ubuntu.sh]

--- NFA_install_scripts/arch_endeavouros.sh
+++ NFA_install_scripts/arch_endeavouros.sh
@@ -5,7 +5,7 @@ sudo pacman -S --needed git pdftk graphviz python tk xz python-pip mpdecimal \
   python-lsp-server python-lark-parser libertinus-font
 
 # LaTeX: very heavy, deactivate for faster install
-sudo pacman -S --needed texlive-binextra texlive-core
+#sudo pacman -S --needed texlive-binextra texlive-core
 
 # get my framework, then run tests
 git clone https://github.com/vincent-hugot/NFA_Framework.git
--- NFA_install_scripts/debian_ubuntu.sh
+++ NFA_install_scripts/debian_ubuntu.sh
@@ -4,7 +4,7 @@ sudo apt install git graphviz pdftk python3-lark fonts-linuxlibertine \
   python3-pylsp idle
 
 # LaTeX: very heavy, deactivate for faster install
-sudo apt-install texlive-extra-utils texlive-full
+#sudo apt-install texlive-extra-utils texlive-full
 
 # get my framework, then run tests
 git clone https://github.com/vincent-hugot/NFA_Framework.git
